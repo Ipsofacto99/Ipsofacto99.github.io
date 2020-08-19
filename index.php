7114c4a735777d3a14e867da261b378d7942bb02
ipsofacto <jose.pablo99@hotmail.com>
2020-08-19 17:27:27 -0500

    Compilador Blog Cantia

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Compilador Blog</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <link rel="stylesheet" id="wp-block-library-css" href="https://cdn.shopify.com/s/files/1/1292/2935/files/blogstyle.css?10498" type="text/css" media="all">
+    <link rel="stylesheet" href="https://cdn.shopify.com/s/files/1/1292/2935/files/animate.css?10481" type="text/css" media="all">
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="favicon/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+<meta name="theme-color" content="#000">
+<link rel="stylesheet" type="text/css" href="estilos.css" media="screen"/> 
+</head>
+
+
+
+   
+        <body>
+        <div class="mdl-layout mdl-js-layout">
+            <header class="mdl-layout__header mdl-layout__header--scroll">
+              <div class="mdl-layout__header-row">
+                <!-- Title -->
+                <span class="mdl-layout-title" style="font-family: Northwell; font-size: 40px;"><img src="https://cdn.shopify.com/s/files/1/1292/2935/files/logonegro.png?v=1581035644" class="cantia-icon" alt="icono logo cantia"></span>
+                <!-- Add spacer, to align navigation to the right -->
+                <div class="mdl-layout-spacer"></div>
+                <!-- Navigation -->
+                <nav class="mdl-navigation">
+                  <a class="mdl-navigation__link" href="">Nuevo Articulo</a>
+                  <a class="mdl-navigation__link" href="">Plantillas</a>
+                  <a class="mdl-navigation__link" href="">Instrucciones</a>
+                  <a class="mdl-navigation__link" href="">Soporte</a>
+                </nav>
+              </div>
+            </header>
+            <div class="mdl-layout__drawer">
+              <span class="mdl-layout-title"><img src="" class="cantia-icon-2" alt="icono logo cantia" style="margin-top: 30px; margin-left: 40px; margin-bottom: 30px;"></span>
+              <nav class="mdl-navigation">
+                <a class="mdl-navigation__link" href="">Nuevo Articulo</a>
+                <a class="mdl-navigation__link" href="">Plantillas</a>
+                <a class="mdl-navigation__link" href="">Instrucciones</a>
+                <a class="mdl-navigation__link" href="">Soporte</a>
+              </nav>
+            </div>
+            <main class="mdl-layout__content">
+              <div class="page-content">
+
+<div class="divisor"></div>
+
+                <div class="jumbotron container animated fadeIn main-card" style="padding: 7%;">
+                    <h1 class="display-4" style="font-family: Northwell; font-size: 100px; justify-content: center;">Compilador  para  el  blog  Cantia</h1>
+                    <p class="lead" style="font-family: Muli; font-size: 24px; font-weight: 400; color: #000; padding-left: 18%; padding-right: auto; text-decoration: underline; padding-top: 25px;">Diseña tu artículo de manera rapida, facil y sencilla.</p>
+
+                  </div>
+
+                  <a href="subindex.php" class="none" id="new-article">
+                  <div class="jumbotron container animated fadeIn">
+                  <h1 class="display-4" style="font-family: Northwell; font-size: 90px;">Crear un nuevo articulo</h1> 
+                    <hr class="my-4">
+                    <p class="lead" style="font-family: Muli; font-size: 20px; font-weight: 400;">¿Estas listo para comenzar? ya puedes crear tus propios articulos de principio a fin. no olvides leer antes las instrucciones</p>
+                  </div></a>
+
+                  <div class="jumbotron container animated fadeIn">
+                    <h1 class="display-4" style="font-family: Northwell; font-size: 90px;">Ver plantillas</h1>
+                    
+                    <hr class="my-4">
+                    <p class="lead" style="font-family: Muli; font-size: 20px; font-weight: 400;">¿Quieres usar una plantilla prediseñada? da clic aquí para configurarla. no olvides leer antes las instrucciones</p>
+
+                  </div>
+                  <div class="jumbotron container animated fadeIn">
+                    <h1 class="display-4" style="font-family: Northwell; font-size: 90px;">Instrucciones de uso</h1>
+                    
+                    <hr class="my-4">
+                    <p class="lead" style="font-family: Muli; font-size: 20px; font-weight: 400;">¿Quieres aprender a usar la herramienta? da clic aquí para comenzar.</p>
+
+                  </div>
+
+                  <div class="jumbotron container animated fadeIn">
+                    <h1 class="display-4" style="font-family: Northwell; font-size: 90px;">Soporte</h1>
+                    
+                    <hr class="my-4">
+                    <p class="lead" style="font-family: Muli; font-size: 20px; font-weight: 400;">¿Necesitas ayuda? da clic aquí para comenzar.</p>
+
+                  </div>
+
+                  
+                
+<div class="divisor"></div>
+
+
+    
+           
+              </div>
+            </main>
+          </div>
+
+
+
+
+      
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+  
